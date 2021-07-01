c41864d5c1de876625dee05aaca9562ccf9d25e3
Lohithaksh <83767927+lohithakshAFK@users.noreply.github.com>
2021-07-01 19:35:29 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,11 +12,32 @@
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-  
+
+  username = localStorage.getItem("username");
+  document.getElementById("username").innerHTML = "Welcome  " + username + "!"; 
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("room name:" + Room_names);
+      row = "<div class='room_name' id=" + Room_names+ " onclick='redirectToRoomName(this.id)'>#" + Room_names +"</div> <hr>"; 
+      document.getElementById("output").innerHTML = row;
       //End code
       });});}
 getData();
+
+function add_room(){
+  room_name = document.getElementById("roomname").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose:"adding room name"
+  });
+  localStorage.setItem("room_name", room_name);
+
+  window.location = "kwitter_page.html";
+}
+
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
